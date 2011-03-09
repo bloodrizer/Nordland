cbf7a9acd32619bf1dcd574ea866592eda5e8bb9
bloodrizer <cheshire.panther@gmail.com>
2011-03-09 08:23:01 +0300

    basic render

--- src/nordland/Main.java
+++ src/nordland/Main.java
@@ -5,17 +5,85 @@
 
 package nordland;
 
+import org.lwjgl.opengl.Display;
+import org.lwjgl.Sys;
+
+import nordland.render.Render;
 /**
  *
  * @author red
  */
 public class Main {
+    
+  int fps;
+  int lastFPS;
+  long lastFrame;
+
+    public static final Render RENDER = Render.getInstance();
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Main main = null;
+
+        main = new Main();
+        main.create();
+
+
+        main.run();
     }
+
+    public void create(){
+        try {
+            RENDER.create();
+        }
+        catch(Exception ex) {
+          //LOGGER.log(Level.SEVERE,ex.toString(),ex);
+        }
+        finally {
+          if(this != null) {
+            this.destroy();
+          }
+        }
+    }
+
+    public void destroy(){
+    
+    }
+
+
+   public void run(){
+      getDelta();
+      lastFPS = (int)Sys.getTime();
+      while(!Display.isCloseRequested()) {
+
+          updateFPS();
+          RENDER.render_all();
+
+          
+          Display.update();
+          Display.sync(60);
+
+        }
+  }
+
+  public int getDelta() {
+    long time = Sys.getTime();
+    int delta = (int) (time - lastFrame);
+    lastFrame = time;
+
+    return delta;
+}
+
+  public void updateFPS() {
+    if (Sys.getTime() - lastFPS > 1000) {
+        Display.setTitle("FPS: " + fps);
+	fps = 0;
+	lastFPS += 1000;
+    }
+    fps++;
+  }
+
 
 }
--- src/nordland/render/Render.java
+++ src/nordland/render/Render.java
@@ -5,10 +5,38 @@
 
 package nordland.render;
 
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
+
+import org.lwjgl.LWJGLException;
 /**
  *
  * @author red
  */
 public class Render {
+    private static final Render INSTANCE = new Render();
 
+    public static final int DISPLAY_HEIGHT = 480;
+    public static final int DISPLAY_WIDTH = 640;
+
+    private Render() {
+
+    }
+
+    public static Render getInstance() {
+        return INSTANCE;
+    }
+
+    public void create() throws LWJGLException {
+        Display.setDisplayMode(new DisplayMode(DISPLAY_WIDTH,DISPLAY_HEIGHT));
+        Display.setFullscreen(false);
+        Display.setTitle("JSKiller");
+        Display.create();
+    }
+
+    //==========================================================================
+
+    public void render_all() {
+        
+    }
 }
